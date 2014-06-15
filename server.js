46fbd3e501c35bbfe629ddda831afe4679dd53c7
JulianChen <julian9925@gmail.com>
2014-06-16 00:37:50 +0800

    Router Test sucesses

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 
 //================== Use Non-blocking function call ================================//
 	http.createServer( function (request, response) {
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for "+ pathname + " Received.");
+		console.log("Request for "+ pathname + " received.");
 
-		route(pathname);
+		var content = route(handle, pathname);
 
 		response.writeHead(200, {"Content-Type": "text/plain"});
-  		response.write("Hello World");
+  		response.write(content);
   		response.end();
 	}).listen(8888);
 //---------------------------------------------------------------------------------//
+
 	console.log("Server has started.");
 }
 
