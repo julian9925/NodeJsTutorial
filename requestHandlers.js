22eda7f1bca11d6865fafa3cd7ba79e1cbe02c4c
JulianChen <julian9925@gmail.com>
2014-06-17 22:15:51 +0800

    Change Block to Non-Block function call

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,20 @@
 var exec = require("child_process").exec;
 
-function start() {
-	console.log("Request handler 'start' was called");
-	return "Hello start";
+function start(response) {
+    console.log("Request handler 'start' was called.");
+
+    exec("ls -lah", function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
 
-function upload() {
-	console.log("Request handler 'upload' was called");
-	return "Hello upload";
+function upload(response) {
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
