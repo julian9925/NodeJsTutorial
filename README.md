ca15959dc39e284b96a71e104718efac616e0959
JulianChen <julian9925@gmail.com>
2014-06-20 01:38:37 +0800

    Finish UploadData(1/2)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ NodeJsTutorial
 Node JS Tutorial : 
 
 This is for the first NodeJS running program. Today is 6/18.
-Let,s celbrate it !!!
+Let,s celebrate it !!!
 
 It is included server.js, requestHandler.js, router.js and server.js
 Simulating the real MVC control.
--- server.js
+++ server.js
@@ -3,10 +3,21 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postdata = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname, response);
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunck) {
+           postdata += postDataChunck;
+            console.log("Received POST data chunck '" +
+                postDataChunck + "'.");
+        });
+
+        route.addListener("end", function () {
+           route(handle, pathname, response, postdata);
+        });
     }
 
     http.createServer(onRequest).listen(8888);
