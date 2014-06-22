06ce66b4c0780708a093e6f150a5bc8bfe1fb141
JulianChen <julian9925@gmail.com>
2014-06-22 17:35:43 +0800

    Finish UploadData(2/2)

--- server.js
+++ server.js
@@ -3,22 +3,10 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postdata = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        request.setEncoding("utf8");
-
-        request.addListener("data", function(postDataChunck) {
-           postdata += postDataChunck;
-            console.log("Received POST data chunck '" +
-                postDataChunck + "'.");
-        });
-
-        route.addListener("end", function () {
-           route(handle, pathname, response, postdata);
-        });
-    }
+        route(handle, pathname, response, request);
+        }
 
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
